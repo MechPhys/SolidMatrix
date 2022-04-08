853035fe9c5993551350ba77b59e6805bdc0b320
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-04-08 02:10:02 -0700

    autocorrelation showing wavelength = 1/momentum

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# SolidMatrix
-Solid particles in a matrix
+# SolidMatrix: Solid particles in a matrix
+
+> "Nature is, above all else, a meticulous accountant"
+
+SolidMatrix is a new approach that is based on discrete particles in a discrete matrix or lattice.  These discrete particles are much better for keeping the accounting tight, but how does a hard little discrete dot move in a seemingly continuous fashion in arbitrary directions?  Interestingly, stochastic motion, where there is a certain probability of taking a step at each point in time, provides a way of smoothing the motion of these discrete particles.  This provides an interesting necessary origin story for the randomness at the heart of quantum physics, and does a great job of keeping the accounting tight.
+
+The resulting physical model is like the deBroglie-Bohm pilot wave theory: the particles interact with a sea of surrounding fields that impart the wave-like properties of QM.
+
+Antonio Sciarretta has developed models based on this principle, in a series of recent papers, showing how a particular formulation of the surrounding field interactions can give rise to otherwise puzzling features of quantum physics.  In particular, these field interactions introduce a novel framework for understanding interference effects, in terms of the cumulative traces left by other particles.  This twist may well provide the critical missing ingredient that finally makes the quantum world make sense: it is well-established that quantum effects manifest only over many repeated samples from "identically prepared" particles, and yet everyone interprets them in terms of physical mechanisms operating only on a single trial, associated with a single isolated particle.  This fundamental disconnect has great potential for resolving paradoxes.
+
+Here, the approach converges with the research on the zero-point field, as developed by Santos, Marshall and others, which is synonymous with the virtual particles captured in the path-integral formulation of Feynmann and his famous diagrams.  The field is very much "alive", teaming with random "quantum foam", and this fabric of "empty" space likely plays an essential role in explaining various puzzling features of QM.  In the stochastic electrodynamics (SED) approach, the field is the exclusive source of stochastic behavior.  However, the isotrophic motion of discrete particles requires intrinsic stochasticity, thus freeing the field to play a less strongly-constrained role.
+
+In short, we have several different models of the quantum field: the original S field formulation from Bohm, the path integral of Feynmann (which is divergent in general), the second quantized Fourier space formulation of quantum field theory, the SED / ZPF field, and Sciarretta's specific "trace" field.  Between all of these models, some kind of consistent framework should be derivable.
+
+Sciarretta's specific formulation has some strange features, including that the interaction depends on the specific lifetime of the particle.  However, in practice the field is initialized to an equilibrium distribution, based on the configuration.
+
+Thus, in the end, the field "senses" the configuration and imparts corresponding forces onto the particle, which is the same qualitative account as the pilot wave framework.  Furthermore, Sciarretta's model has an appealing conserving exchange process in the particle's interaction with the field.
+
+Some other challenges for the field model include accounting for the space-time distortion effects of special relativity, which are so elegantly captured in the wave dynamics.
+
+Although hard discrete particles make sense for fermions, which obey strict conservation principles and the Pauli exclusion principle, bosons like the troubling photon have very different properties.  They have no mass or charge, accumulate in graded amounts with fully linear superposition, always move at the speed of light, and the relevant wavelengths can be astronomically large compared to the Compton wavelength of even the lightest particles.
+
+![Stochastic Origin of Quantum Momentum / Frequency Relationship](figs/fig_asmom5_0_autoc.png?raw=true "Stochastic Origin of Quantum Momentum / Frequency Relationship.  The momentum on the left is 0.5c while on the right is 0. The distribution of position is on the vertical axis, while time is on the horizontal axis, with each point centered at the origin in the center (i.e., the temporal autocorrelation function).  The variance on the left is half of that on the right.")
+
+Nelson (1966) shows how brownian stochastic motion gives rise to the Schrodinger equation: a central intuition is that slow drift produces a wide cloud of space where particle could be, corresponding to a long wavelength in the probability cloud that the Schrodinger wave function describes.  However, when the particle has high momentum, it moves more deterministically in the given direction, resulting in a narrower range of variance around the particle's mean trajectory, resulting in a narrower effective wavelength along the direction of motion.  This is illustrated in the above figure.
+
+## Differences Between Photons and Real Particles
+
+Photons differ in so many ways from electrons and other particles, it is hard to imagine that both could be described by the same underlying mechanistic model.  Rest mass, charge, conserved number, fractional spin, etc.  In short, it is difficult to see how photons could be fit within the same paradigm.  The simplest approach at this point is to implement EM using classical Maxwell's equations.
+
+# References
+
+See https://www.zotero.org/groups/2525742/mechphys/library for an extensive library of relevant papers.
+
+* O’Reilly, R. C. (2011). Surely You Must All be Joking: An Outsider’s Critique of Quantum Physics. ArXiv:1109.0880]. http://arxiv.org/abs/1109.0880
+
+* Santos E. (2015). Towards a realistic interpretation of quantum mechanics providing a model of the physical world, Foundations of Science 20(4), 357–386. https://doi.org/10.1007/s10699-014-9366-y
+
+* Sciarretta, A. (2018). A Local-Realistic Model of Quantum Mechanics Based on a Discrete Spacetime. Foundations of Physics, 48(1), 60–91. https://doi.org/10.1007/s10701-017-0129-9
+
+* Sciarretta, A. (2018). A Local-Realistic Model of Quantum Mechanics Based on a Discrete Spacetime (Extended version). Foundations of Physics, 48(1), 60–91. http://arxiv.org/abs/1712.03227
+
+* Sciarretta, A. (2021). A local-realistic quantum mechanical model of spin and spin entanglement. International Journal of Quantum Information, 19(01), 2150006. https://doi.org/10.1142/S0219749921500064
+
+
--- sims/smatrix1/smatrix.go
+++ sims/smatrix1/smatrix.go
@@ -6,6 +6,8 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
 
 	"github.com/emer/emergent/edge"
@@ -17,6 +19,7 @@ import (
 	_ "github.com/emer/etable/etview" // include to get gui views
 	"github.com/goki/gi/gi"
 	"github.com/goki/gi/gimain"
+	"github.com/goki/ki/ints"
 	"github.com/goki/mat32"
 )
 
@@ -37,16 +40,18 @@ const LogPrec = 4
 
 // Params holds the particle params
 type Params struct {
-	Dims      int `desc:"number of dimensions"`
-	Size      int `desc:"size of world along each dim"`
-	Particles int `desc:"n of particles"`
-	Mass      float32
+	Dims      int     `desc:"number of dimensions"`
+	Size      int     `desc:"size of world along each dim"`
+	StatWin   int     `desc:"time window in which to track stats"`
+	Particles int     `desc:"n of particles"`
+	Mass      float32 `desc:"particle mass"`
 	Trace     float32 `desc:"1-rate of decay for display trace"`
 }
 
 func (pr *Params) Defaults() {
 	pr.Dims = 2
-	pr.Size = 100
+	pr.Size = 2000
+	pr.StatWin = 100
 	pr.Particles = 1
 	pr.Mass = 1
 	pr.Trace = 0.9
@@ -54,13 +59,14 @@ func (pr *Params) Defaults() {
 
 // Sim holds the params, table, etc
 type Sim struct {
-	Params    Params       `view:"inline" desc:"main params"`
-	Mom0      mat32.Vec2   `desc:"initial momemntum"`
-	TimeSteps int          `desc:"total number of time steps to take"`
-	State     estats.Stats `desc:"state and stats"`
-	Logs      elog.Logs    `desc:"logs"`
-	GUI       egui.GUI     `view:"-" desc:"manages all the gui elements"`
-	NoGui     bool         `view:"-" desc:"if true, runing in no GUI mode"`
+	Params   Params       `view:"no-inline" desc:"main params"`
+	Mom0     mat32.Vec2   `desc:"initial momemntum"`
+	NRuns    int          `desc:"number of runs resetting to start between"`
+	RunSteps int          `desc:"total number of time steps to take per Run"`
+	State    estats.Stats `desc:"state and stats"`
+	Logs     elog.Logs    `desc:"logs"`
+	GUI      egui.GUI     `view:"-" desc:"manages all the gui elements"`
+	NoGui    bool         `view:"-" desc:"if true, runing in no GUI mode"`
 }
 
 // TheSim is the overall state for this simulation
@@ -68,15 +74,18 @@ var TheSim Sim
 
 func (ss *Sim) Defaults() {
 	ss.Params.Defaults()
+	ss.Mom0.Set(0.5, 0)
 }
 
 // Config configures all the elements using the standard functions
 func (ss *Sim) Config() {
 	ss.Defaults()
-	ss.TimeSteps = 1000
+	ss.NRuns = 20
+	ss.RunSteps = 1000
 	ss.Update()
 	ss.ConfigState()
 	ss.ConfigLogs()
+	ss.Init()
 }
 
 // Update updates computed values
@@ -97,15 +106,71 @@ func (ss *Sim) ConfigState() {
 	ss.State.SetIntTensor("Pos", pos)
 	mom := etensor.NewFloat32([]int{pr.Particles, pr.Dims}, nil, []string{"Ps", "Dims"})
 	ss.State.SetF32Tensor("Mom", mom)
+
+	tr.SetMetaData("fix-max", "false")
+	tr.SetMetaData("grid-min", "1")
+	pos.SetMetaData("grid-min", "1")
+
+	ss.State.SetInt("Run", 0)
+	ss.State.SetInt("Time", 0)
+	ss.State.SetInt("PosX", 0)
+	ss.State.SetInt("PosY", 0)
 }
 
 // ConfigLogs configures tables and logs
 func (ss *Sim) ConfigLogs() {
+	ss.ConfigLogItems()
 	ss.Logs.CreateTables()
+	ss.Logs.SetContext(&ss.State, nil)
+	ss.Logs.SetMeta(elog.Train, elog.Cycle, "XAxisCol", "Time")
+}
+
+func (ss *Sim) ConfigLogItems() {
+	ss.Logs.AddItem(&elog.Item{
+		Name: "Run",
+		Type: etensor.INT64,
+		Plot: elog.DFalse,
+		Write: elog.WriteMap{
+			elog.Scope(elog.Train, elog.Cycle): func(ctx *elog.Context) {
+				ctx.SetStatInt("Run")
+			}}})
+	ss.Logs.AddItem(&elog.Item{
+		Name: "Time",
+		Type: etensor.INT64,
+		Plot: elog.DFalse,
+		Write: elog.WriteMap{
+			elog.Scope(elog.Train, elog.Cycle): func(ctx *elog.Context) {
+				ctx.SetStatInt("Time")
+			}}})
+	ss.Logs.AddItem(&elog.Item{
+		Name: "PosX",
+		Type: etensor.INT64,
+		Plot: elog.DFalse,
+		Write: elog.WriteMap{
+			elog.Scope(elog.Train, elog.Cycle): func(ctx *elog.Context) {
+				ctx.SetStatInt("PosX")
+			}}})
+	ss.Logs.AddItem(&elog.Item{
+		Name: "PosY",
+		Type: etensor.INT64,
+		Plot: elog.DFalse,
+		Write: elog.WriteMap{
+			elog.Scope(elog.Train, elog.Cycle): func(ctx *elog.Context) {
+				ctx.SetStatInt("PosY")
+			}}})
 }
 
 // Init inits
 func (ss *Sim) Init() {
+	ss.InitState()
+	ss.State.SetInt("Run", 0)
+	ss.State.SetInt("Time", 0)
+	dt := ss.Logs.Table(elog.Train, elog.Cycle)
+	dt.SetNumRows(ss.NRuns * ss.RunSteps)
+}
+
+// InitState inits state between runs
+func (ss *Sim) InitState() {
 	pr := &ss.Params
 	cur := ss.State.F32Tensor("Cur")
 	tr := ss.State.F32Tensor("Tr")
@@ -118,6 +183,7 @@ func (ss *Sim) Init() {
 	pos.Set([]int{0, 1}, pr.Size/2)
 	mom.Set([]int{0, 0}, ss.Mom0.X)
 	mom.Set([]int{0, 1}, ss.Mom0.Y)
+	ss.UpdtStats()
 }
 
 func Move(v float32) int {
@@ -141,6 +207,68 @@ func Move(v float32) int {
 func UpdtPos(p, s, sz int) int {
 	p, _ = edge.Edge(p+s, sz, true) // wrap
 	return p
+}
+
+// UpdtStats
+func (ss *Sim) UpdtStats() {
+	pos := ss.State.IntTensor("Pos")
+	px := pos.Value([]int{0, 0})
+	py := pos.Value([]int{0, 1})
+	ss.State.SetInt("PosX", px)
+	ss.State.SetInt("PosY", py)
+}
+
+// Log is the main logging function, handles special things for different scopes
+func (ss *Sim) Log(mode elog.EvalModes, time elog.Times, row int) {
+	// dt := ss.Logs.Table(mode, time)
+	//	row := dt.Rows
+	ss.Logs.LogRow(mode, time, row) // also logs to file, etc
+	if time == elog.Cycle {
+		ss.GUI.UpdateCyclePlot(elog.Train, row)
+	} else {
+		ss.GUI.UpdatePlot(mode, time)
+	}
+}
+
+// DoStats
+func (ss *Sim) DoStats() {
+	twin := ss.Params.StatWin
+	dt := ss.Logs.Table(elog.Train, elog.Cycle)
+	mmax := mat32.Max(ss.Mom0.X, ss.Mom0.Y)
+	mmax = mat32.Max(0.2, mmax)
+	maxt := int(mat32.Ceil(float32(twin) * mmax))
+	nt := 2*maxt + 1
+
+	st := etensor.NewFloat32([]int{2, nt, nt}, nil, []string{"XY", "D", "T"})
+
+	var max float32
+	for ri := 0; ri < ss.NRuns; ri++ {
+		ro := ri * ss.RunSteps
+		for j := twin; j < ss.RunSteps-twin; j++ {
+			cx := dt.CellFloat("PosX", ro+j)
+			cy := dt.CellFloat("PosY", ro+j)
+			for i := -twin; i <= twin; i++ {
+				d := ro + j + i
+				ti := ints.ClipInt(int(mat32.Round(float32(i)*mmax))+maxt, 0, nt)
+				tx := dt.CellFloat("PosX", d)
+				ty := dt.CellFloat("PosY", d)
+				dx := ints.ClipInt(maxt+int(math.Round(tx-cx)), 0, nt)
+				mx := st.Add([]int{0, dx, ti}, 1)
+				dy := ints.ClipInt(maxt+int(math.Round(ty-cy)), 0, nt)
+				my := st.Add([]int{1, dy, ti}, 1)
+				max = mat32.Max(max, mx)
+				max = mat32.Max(max, my)
+			}
+		}
+		fmt.Printf("Stat run: %d\n", ri)
+		if ss.GUI.StopNow {
+			break
+		}
+	}
+	st.SetMetaData("max", fmt.Sprintf("%g", .8*max))
+	st.SetMetaData("grid-min", "1")
+	st.SetMetaData("grid-fill", "1")
+	ss.State.SetF32Tensor("Auto", st)
 }
 
 // Step runs one update
@@ -170,7 +298,10 @@ func (ss *Sim) Step() {
 	pos.Set([]int{0, 1}, py)
 
 	cur.Set([]int{py, px}, 1)
-	tr.Set([]int{py, px}, tr.Value([]int{py, px})+1)
+	tr.Add([]int{py, px}, 1)
+
+	ss.State.SetInt("Time", ss.State.Int("Time")+1)
+	ss.UpdtStats()
 
 	if ss.GUI.Active {
 		cg := ss.GUI.Grid("Cur")
@@ -183,12 +314,21 @@ func (ss *Sim) Step() {
 // Stop stops running
 func (ss *Sim) Run() {
 	ss.GUI.StopNow = false
-	for t := 0; t < ss.TimeSteps; t++ {
-		ss.Step()
+	row := 0
+	for ri := 0; ri < ss.NRuns; ri++ {
+		ss.InitState()
+		ss.State.SetInt("Run", ri)
+		for ti := 0; ti < ss.RunSteps; ti++ {
+			ss.Step()
+			ss.Log(elog.Train, elog.Cycle, row)
+			row++
+		}
+		fmt.Printf("Run run: %d\n", ri)
 		if ss.GUI.StopNow {
 			break
 		}
 	}
+	ss.DoStats()
 	ss.Stopped()
 }
 
@@ -206,7 +346,7 @@ func (ss *Sim) Stopped() {
 func (ss *Sim) ConfigGui() *gi.Window {
 	title := "SolidMatrix"
 	ss.GUI.MakeWindow(ss, "smatrix", title, `SolidMatrix model. See <a href="https://github.com/MechPhys/SolidMatrix">GitHub</a>.</p>`)
-	ss.GUI.CycleUpdateInterval = 10
+	ss.GUI.CycleUpdateInterval = 100
 
 	cur := ss.State.F32Tensor("Cur")
 	tr := ss.State.F32Tensor("Tr")
@@ -220,6 +360,8 @@ func (ss *Sim) ConfigGui() *gi.Window {
 	tg.SetStretchMax()
 	ss.GUI.SetGrid("Tr", tg)
 	tg.SetTensor(tr)
+
+	ss.GUI.AddPlots(title, &ss.Logs)
 
 	ss.GUI.AddToolbarItem(egui.ToolbarItem{Label: "Init", Icon: "update",
 		Tooltip: "Initialize state, and start over.  Also applies current params.",
